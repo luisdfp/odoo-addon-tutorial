204f68379d67220509e822a917f0c41e529cfb73
user0 <user0@web-debian>
2018-02-16 12:18:09 -0400

    Replace the ad-hoc Session workflow by a real workflow

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -28,6 +28,7 @@
         # 'security/ir.model.access.csv',
         'views/openacademy.xml',
         'views/partner.xml',
+        'views/session_workflow.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -78,7 +78,7 @@ class Session(models.Model):
         ('draft', "Draft"),
         ('confirmed', "Confirmed"),
         ('done', "Done"),
-    ], default='draft')
+    ])
 
     @api.multi
     def action_draft(self):
