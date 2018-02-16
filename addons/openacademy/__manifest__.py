9736ba26f66ee7f0901d2ce005fb99ea25107188
user0 <user0@web-debian>
2018-02-16 15:22:56 -0400

    Create a group OpenAcademy / Manager with full access to all OpenAcademy models; Make Session and Course readable by all users

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -25,7 +25,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'security/ir.model.access.csv',
         'views/openacademy.xml',
         'views/partner.xml',
         'views/session_workflow.xml'
